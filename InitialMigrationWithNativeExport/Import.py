07d623e76a6cbf97dede1abb702fdaa92485b190
Adrian Susanto <adrian.susanto@traveloka.com>
2021-06-23 18:23:14 +0700

    add db and table name

--- InitialMigrationWithNativeExport/Import.py
+++ InitialMigrationWithNativeExport/Import.py
@@ -31,23 +31,26 @@ job.init(args['JOB_NAME'], args)
 # Initialize the Dynamic frame using Glue Data Catalog DB and Table.  Replace <GlueDatabaseName> and <GlueTableName> with Glue DB and table names respectively
 
 Source = glueContext.create_dynamic_frame.from_catalog(
-    database="< GlueDatabaseName >", table_name="< GlueTableName >", transformation_ctx="Source")
+    database="ft-versioning-migrated", table_name="taboneuyfa5m3jpmt7x4rhyqky_json_gz", transformation_ctx="Source")
 
 
 # Map the source field names and data types to target values. The target values should be exactly the same as the source DyanmoDB table values
 Mapped = ApplyMapping.apply(frame=Source, mappings=[
-    ("item.ID.S", "string", "ID", "string"),
-    ("item.date.M", "map", "date", "map"),
-    ("item.location.M.lng.S", "string", "location.lng", "string"),
-    ("item.location.M.lat.S", "string", "location.lat", "string")],
+    ("item.createdAt.N", "string", "createdAt", "string"),
+    ("item.expiredAt.N", "string", "expiredAt", "string"),
+    ("item.id.S", "string", "id", "string"),
+    ("item.owner.S", "string", "owner", "string"),
+    ("item.retry.N", "string", "retry", "string"),
+    ("item.status.S", "string", "status", "string"),
+    ("item.version.N", "string", "version", "string")],
     transformation_ctx="Mapped")
 
 # Write to target DynamoDB table. Replace <TargetRegion> and <TargetDynamoDBTable> with region and table name respectively
 glueContext.write_dynamic_frame_from_options(
     frame=Mapped,
     connection_type="dynamodb",
-    connection_options={"dynamodb.region": "< TargetRegion >",
-                        "dynamodb.output.tableName": "< TargetDynamoDBTable >",
+    connection_options={"dynamodb.region": "ap-southeast-1",
+                        "dynamodb.output.tableName": "ft-versioning-migrated",
                         "dynamodb.throughput.write.percent": "1.0"
                         }
 )
