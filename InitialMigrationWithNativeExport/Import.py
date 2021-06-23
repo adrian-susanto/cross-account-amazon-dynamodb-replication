d7aca9a1fc9dc55ba771ea9853cf69bb0d911d64
Adrian Susanto <adrian.susanto@traveloka.com>
2021-06-23 18:17:05 +0700

    adjust script for ft-versioning-stg testing

--- InitialMigrationWithNativeExport/Import.py
+++ InitialMigrationWithNativeExport/Import.py
@@ -21,34 +21,34 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 
 
-args = getResolvedOptions(sys.argv, [‘JOB_NAME’])
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args[‘JOB_NAME’], args)
+job.init(args['JOB_NAME'], args)
 
 # Initialize the Dynamic frame using Glue Data Catalog DB and Table.  Replace <GlueDatabaseName> and <GlueTableName> with Glue DB and table names respectively
 
-Source = glueContext.create_dynamic_frame.from_catalog(database = “<GlueDatabaseName>”, table_name = “<GlueTableName>”, transformation_ctx = “Source”)
+Source = glueContext.create_dynamic_frame.from_catalog(
+    database="< GlueDatabaseName >", table_name="< GlueTableName >", transformation_ctx="Source")
 
 
 # Map the source field names and data types to target values. The target values should be exactly the same as the source DyanmoDB table values
-Mapped = ApplyMapping.apply(frame = Source, mappings = [
-                        (“item.ID.S”, “string”, “ID”, “string”), 
-                        (“item.date.M”, “map”, “date”, “map”),  
-                        (“item.location.M.lng.S”, “string”, “location.lng”, “string”), 
-                        (“item.location.M.lat.S”, “string”, “location.lat”, “string”)],
-                        transformation_ctx = “Mapped”)
+Mapped = ApplyMapping.apply(frame=Source, mappings=[
+    ("item.ID.S", "string", "ID", "string"),
+    ("item.date.M", "map", "date", "map"),
+    ("item.location.M.lng.S", "string", "location.lng", "string"),
+    ("item.location.M.lat.S", "string", "location.lat", "string")],
+    transformation_ctx="Mapped")
 
 # Write to target DynamoDB table. Replace <TargetRegion> and <TargetDynamoDBTable> with region and table name respectively
 glueContext.write_dynamic_frame_from_options(
     frame=Mapped,
-    connection_type=“dynamodb”,
-    connection_options={
-        “dynamodb.region”: “<TargetRegion>”,
-        “dynamodb.output.tableName”: “<TargetDynamoDBTable>”,
-        “dynamodb.throughput.write.percent”: “1.0”
-    }
+    connection_type="dynamodb",
+    connection_options={"dynamodb.region": "< TargetRegion >",
+                        "dynamodb.output.tableName": "< TargetDynamoDBTable >",
+                        "dynamodb.throughput.write.percent": "1.0"
+                        }
 )
 job.commit()
